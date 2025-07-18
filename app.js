f99cc706471681b8ab7184a6551c090703d0a092
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-18 11:24:08 +0530

    configure database client

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+let cachedDb = null;
+
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+  const db = await client.connect();
+  cachedDb = db;
+  return db;
+}
+
 export const handler = async (event) => {
   console.log("Received event:", JSON.stringify(event));
   return {
