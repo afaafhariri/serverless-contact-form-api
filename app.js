9f192f0ea68ab109c81f2312aa260e3b471b19f3
Afaaf Hariri <afaafhariri@icloud.com>
2025-07-18 18:34:35 +0530

    remove dotenv config

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import { MongoClient } from "mongodb";
-
-dotenv.config();
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
